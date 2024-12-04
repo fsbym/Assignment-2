afdf5e8c88928cf8aa489904e589c69549857dda
fsbym <37413822+fsbym@users.noreply.github.com>
2024-12-03 20:54:44 -0500

    initial

---
+++ backend/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+// In the root directory: npm install --save-dev @nomicfoundation/hardhat-ethers ethers
+require("@nomicfoundation/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  defaultNetwork: "sepolia",
+  solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url:
+        "https://eth-sepolia.g.alchemy.com/v2/" +
+        process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.METAMASK_ACCOUNT_PRIVATE_KEY],
+    },
+  },
+};
